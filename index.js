0e398da15f24ce33e54ec4faeff7fe44b89d71b7
Emmanuel <emmanuelkibet186@gmail.com>
2023-07-01 22:56:56 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+destructivelyAppendCat("Ralph");
+
+funtion destructiveluPrependCat(name){
+    return cats.unshift(name);
+}
+destructiveluPrependCat("BOB");
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+    return [...cats, name];
+}
+appendCat("Broom");
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+prependCat("Arnold");
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+removeLastCat();
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
